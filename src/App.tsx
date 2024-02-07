59859cf7fbd56d3158a6900651bbe1b9821e4a31
Murilo Missura <murilo@Mac-mini-de-Murilo.local>
2024-02-07 20:41:46 -0300

    change names clients

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ export const App = () => {
             target="_blank"
             className="bg-white text-black py-3 px-10 text-center mt-12 flex justify-center items-center gap-3 rounded-xl mb-12"
             >
-              Imobiliaria 1
+              Imobiliária Hoouse
             </a>
             {/* Sua imobiliária aqui */}
 
@@ -26,7 +26,7 @@ export const App = () => {
             target="_blank"
             className="bg-white text-black py-3 px-10 text-center mt-12 flex justify-center items-center gap-3 rounded-xl mb-12"
             >
-              Imobiliaria 2
+              Imobiliária Rio
             </a>
             {/* Sua imobiliária aqui */}
 
@@ -36,7 +36,7 @@ export const App = () => {
             target="_blank"
             className="bg-white text-black py-3 px-10 text-center mt-12 flex justify-center items-center gap-3 rounded-xl mb-12"
             >
-              Imobiliaria 3
+              Imobiliária Reall
             </a>
             {/* Sua imobiliária aqui */}
 
@@ -46,7 +46,7 @@ export const App = () => {
             target="_blank"
             className="bg-white text-black py-3 px-10 text-center mt-12 flex justify-center items-center gap-3 rounded-xl mb-12"
             >
-              Imobiliaria 4
+              Imobiliária Fortaleza
             </a>
             {/* Sua imobiliária aqui */}
 
